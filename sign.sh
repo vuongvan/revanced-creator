a11c6e0ea6bde3dd0bd9ab9e86a5f56b7d561be6
Vuong Mai <maivanvuong@gmail.com>
2022-08-06 21:13:00 +0700

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,14 +1,6 @@
 printf "Download apksigner"
-
-dl="curl -OL https://github.com/vuongvan/revanced-creator/releases/download/v0.1/apksigner.zip"
-$dl
-dl1="curl -OL https://github.com/vuongvan/revanced-creator/releases/download/v0.1/apksigner.jar"
-$dl1
-
-printf "Unzip apksigner\n"
-unzip apksigner.zip
 printf "Resign Apk\n"
 reapk=$(find . -type f -name "revanced-*-non-root.apk")
-base_cmd="java -jar ApkSigner.jar sign  --key key.pk8 --cert key.pem  --v4-signing-enabled false --out $reapk-resigned.apk $reapk"
+base_cmd="java -jar apksigner.jar sign  --key key.pk8 --cert key.pem  --v4-signing-enabled false --out $reapk-resigned.apk $reapk"
 $base_cmd
 printf "Signed Apk\n"
