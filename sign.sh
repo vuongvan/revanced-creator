bea093d571b8c69da2a16cede923046190769afc
Vuong Mai <maivanvuong@gmail.com>
2022-08-06 21:32:14 +0700

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,5 +1,8 @@
 printf "Resign Apk\n"
 reapk=$(find . -type f -name "revanced-*-non-root.apk")
+printf "$reapk\n"
+reapk=${reapk:2}
+printf "$reapk\n"
 base_cmd="java -jar apksigner.jar sign  --key key.pk8 --cert key.pem  --v4-signing-enabled false --out $reapk-resigned.apk $reapk"
 $base_cmd
 printf "Signed Apk\n"
