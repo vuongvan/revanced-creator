aa83bfe9869271579870e9f9f79079fd025c316e
Vuong Mai <maivanvuong@gmail.com>
2022-08-06 22:16:45 +0700

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -5,6 +5,6 @@ reapk="revanced-17.29.34-non-root.apk"
 #printf "$reapk\n"
 #reapk=${reapk:2}
 #printf "$reapk\n"
-base_cmd="java -jar apksigner.jar sign  --key key.pk8 --cert key.pem  --v4-signing-enabled false --out $reapk-resigned.apk $reapk"
+base_cmd="java -jar apksigner.jar sign  --key key.pk8 --cert key.pem  --v4-signing-enabled false --out /home/runner/work/revanced-creator/revanced-creator/revanced-17.29.34-non-root-signed.apk /home/runner/work/revanced-creator/revanced-creator/revanced-17.29.34-non-root.apk"
 $base_cmd
 printf "Signed Apk\n"
